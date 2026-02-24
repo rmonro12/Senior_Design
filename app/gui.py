43d56c9cec9a8345f1305bd37bb7180adbcc5916
Ryan Monroe <rmonro12@charlotte.edu>
2026-02-23 22:18:05 -0500

    Uncommented lines that were removed for testing

--- app/gui.py
+++ app/gui.py
@@ -4,16 +4,16 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-#from .serial_comm import ProjectorSerial
-#from .controller import ProjectorController
+from .serial_comm import ProjectorSerial
+from .controller import ProjectorController
 from Projector_Control.display.scene import SceneController # !! WIP !!
 
 app = FastAPI()
 template = Jinja2Templates(directory="app/templates")
 
 # --- Initialize hardware once ---
-#serial_iface = ProjectorSerial(port="COM3")  # change as needed
-#projector = ProjectorController(serial_iface)
+serial_iface = ProjectorSerial(port="COM3")  # change as needed
+projector = ProjectorController(serial_iface)
 scene = SceneController()
 
 @app.get("/", response_class=HTMLResponse)
@@ -22,12 +22,12 @@ def read_root(request: Request):
 
 @app.post("/power/on")
 def power_on():
-    #projector.power_on()
+    projector.power_on()
     return {"status": "Power ON command sent"}
 
 @app.post("/power/off")
 def power_off():
-    #projector.power_off()
+    projector.power_off()
     return {"status": "Power OFF command sent"}
 
 @app.post("/move/{direction}")
@@ -38,4 +38,4 @@ def move(direction: str):
 @app.on_event("shutdown")
 def shutdown_event():
     print("Closing serial connection...")
-    #serial_iface.close()
+    serial_iface.close()
